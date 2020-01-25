ac772b9f88f0d7d0dd11f120f7d17c6821069db1
Leandro Shindi Ekamoto <leandro_shindi@hotmail.com>
2020-01-25 15:56:26 -0300

    Remover chaves duplicadas nos testes. Estava dando erro quando executava todos os testes ao mesmo tempo

--- Shindi.CoisasAFaz.Testes/GerenciaPrazoDasTarefasHandlerExecute.cs
+++ Shindi.CoisasAFaz.Testes/GerenciaPrazoDasTarefasHandlerExecute.cs
@@ -18,26 +18,26 @@ namespace Shindi.CoisasAFaz.Testes
         public void QuandoTarefasEstiveremAtrasadasDeveMudarSeuStatus()
         {
             //arrange
-            var compCateg = new Categoria(1, "Compras");
-            var casaCateg = new Categoria(2, "Casa");
-            var trabCateg = new Categoria(3, "Trabalho");
-            var saudCateg = new Categoria(4, "Saúde");
-            var higiCateg = new Categoria(5, "Higiene");
+            var compCateg = new Categoria(300, "Compras");
+            var casaCateg = new Categoria(302, "Casa");
+            var trabCateg = new Categoria(303, "Trabalho");
+            var saudCateg = new Categoria(304, "Saúde");
+            var higiCateg = new Categoria(305, "Higiene");
 
             var tarefas = new List<Tarefa>
             {
                 //atrasadas a partir de 1/1/2019
-                new Tarefa(1, "Tirar lixo", casaCateg, new DateTime(2018,12,31), null, StatusTarefa.Criada),
-                new Tarefa(4, "Fazer o almoço", casaCateg, new DateTime(2017,12,1), null, StatusTarefa.Criada),
-                new Tarefa(9, "Ir à academia", saudCateg, new DateTime(2018,12,31), null, StatusTarefa.Criada),
-                new Tarefa(7, "Concluir o relatório", trabCateg, new DateTime(2018,5,7), null, StatusTarefa.Pendente),
-                new Tarefa(10, "Beber água", saudCateg, new DateTime(2018,12,31), null, StatusTarefa.Criada),
+                new Tarefa(401, "Tirar lixo", casaCateg, new DateTime(2018,12,31), null, StatusTarefa.Criada),
+                new Tarefa(404, "Fazer o almoço", casaCateg, new DateTime(2017,12,1), null, StatusTarefa.Criada),
+                new Tarefa(409, "Ir à academia", saudCateg, new DateTime(2018,12,31), null, StatusTarefa.Criada),
+                new Tarefa(407, "Concluir o relatório", trabCateg, new DateTime(2018,5,7), null, StatusTarefa.Pendente),
+                new Tarefa(410, "Beber água", saudCateg, new DateTime(2018,12,31), null, StatusTarefa.Criada),
                 //dentro do prazo em 1/1/2019
-                new Tarefa(8, "Comparecer à reunião", trabCateg, new DateTime(2018,11,12), new DateTime(2018,11,30), StatusTarefa.Concluida),
-                new Tarefa(2, "Arrumar a cama", casaCateg, new DateTime(2019,4,5), null, StatusTarefa.Criada),
-                new Tarefa(3, "Escovar os dentes", higiCateg, new DateTime(2019,1,2), null, StatusTarefa.Criada),
-                new Tarefa(5, "Comprar presente pro João", compCateg, new DateTime(2019,10,8), null, StatusTarefa.Criada),
-                new Tarefa(6, "Comprar ração", compCateg, new DateTime(2019,11,20), null, StatusTarefa.Criada),
+                new Tarefa(408, "Comparecer à reunião", trabCateg, new DateTime(2018,11,12), new DateTime(2018,11,30), StatusTarefa.Concluida),
+                new Tarefa(402, "Arrumar a cama", casaCateg, new DateTime(2019,4,5), null, StatusTarefa.Criada),
+                new Tarefa(403, "Escovar os dentes", higiCateg, new DateTime(2019,1,2), null, StatusTarefa.Criada),
+                new Tarefa(405, "Comprar presente pro João", compCateg, new DateTime(2019,10,8), null, StatusTarefa.Criada),
+                new Tarefa(406, "Comprar ração", compCateg, new DateTime(2019,11,20), null, StatusTarefa.Criada),
             };
 
             var options = new DbContextOptionsBuilder<DbTarefasContext>()
@@ -72,14 +72,16 @@ namespace Shindi.CoisasAFaz.Testes
                     Categoria = categoria, 
                     Titulo="ToDo", 
                     ConcluidaEm= DateTime.Now,
-                    Prazo=new DateTime(2019,9,21)
+                    Prazo=new DateTime(2019,9,21),
+                    Id = 200
                 },
                 new Tarefa()
                 {
                     Categoria = categoria,
                     Titulo="Do",
                     ConcluidaEm= DateTime.Now,
-                    Prazo=new DateTime(2019,7,21)
+                    Prazo=new DateTime(2019,7,21),
+                    Id = 201
                 }
             };
 
